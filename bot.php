db05c4aae1ae7730d929ba9c690c3195cb212b73
Jarin Charoensiri <tapootumchannel@gmail.com>
2016-10-13 22:58:04 +0700

    add JSON reply

--- bot.php
+++ bot.php
@@ -1,3 +1,49 @@
 <?php
-echo "Tapootum Bot";
+$access_token = 'Xqs5qIxucIVe7ZGk5Cx1uiLV45hwwiGSxFuIPSgXN6d0pLBXhvrhWKMYz7ftdxuzwEfu3k8/8Mj8z4vrCYOlY5YJj/dOe4/qbfcdm6dRYxJbphD23UzrSWAft4PfPGMl8gWrXLd3c7Yr64NwicfOgwdB04t89/1O/w1cDnyilFU=';
+
+// Get POST body content
+$content = file_get_contents('php://input');
+// Parse JSON
+$events = json_decode($content, true);
+// Validate parsed JSON data
+if (!is_null($events['events'])) {
+	// Loop through each event
+	foreach ($events['events'] as $event) {
+		// Reply only when message sent is in 'text' format
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+			// Get text sent
+			$text = $event['message']['text'];
+			// Get replyToken
+			$replyToken = $event['replyToken'];
+
+			// Build message to reply back
+			$messages = [
+				'type' => 'text',
+				'text' => $text
+			];
+
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";
+		}
+	}
+}
+echo "OK";
+
 ?>
